e2720c092158189b26e540476b13bd5e793e942b
Zain Shafique <zshafiqu@ucsc.edu>
2020-02-14 19:27:12 -0800

    update test urls for 112

--- parse_midterms.py
+++ parse_midterms.py
@@ -28,11 +28,11 @@ BASE_DIR = ""
 TESTS = []
 if sys.argv[4] == "112":
     BASE_DIR = "https://www2.ucsc.edu/courses/cse112-wm/:/Old-Exams/"
-    TESTS = ["cmps112-2017q2-midterm.tt",
-             "cmps112-2017q4-midterm.tt",
+    TESTS = ["cmps112-2017q4-midterm.tt",
              "cmps112-2018q1-midterm.tt",
              "cmps112-2018q2-midterm.tt",
-             "cmps112-2018q4-midterm.tt ",
+             "cmps112-2018q4-midterm.tt",
+             "cmps112-2019q1-midterm.tt",
             ]
 elif sys.argv[4] == "104a":
     BASE_DIR = "https://www2.ucsc.edu/courses/cmps104a-wm/:/Old-Exams/"
